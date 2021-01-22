68a0465000453fc6b3a179d658c39ece389870fa
joaoluisro <joaoluisok@gmail.com>
2021-01-22 14:51:22 -0300

    added scraping

--- scrape.py
+++ scrape.py
@@ -1,40 +1,69 @@
 import requests
+import time
+
 import datetime
+
 from bs4 import BeautifulSoup
-from requests_html import HTMLSession
+
+from selenium import webdriver
+
+import re
+
+def connect_and_fetch(URL):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--incognito")
+    options.add_argument("--headless")
+    driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=options)
+
+    driver.get(URL)
+
+    SCROLL_PAUSE_TIME = 0.5
+
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    page_source = driver.page_source
+
+    soup = BeautifulSoup(page_source, "lxml")
+    matches = soup.select('div[class="EventDate"]')
+
+    return matches
+
+def update_csv(matches):
+
+    with open('recorded_matches.csv', 'w') as file:
 
 monthdict = {
-    1 : "Janeiro",
-    2 : "Fevereiro",
-    3 : "Março",
-    4 : "Abril",
-    5 : "Maio",
-    6 : "Junho",
-    7 : "Julho",
-    8 : "Agosto",
-    9 : "Setembro",
-    10: "Outubro",
-    11: "Novembro",
-    12: "Dezembro"
-    }
-
-
-
+    1: "janeiro",
+    2: "fevereiro",
+    3: "março",
+    4: "abril",
+    5: "maio",
+    6: "junho",
+    7: "julho",
+    8: "agosto",
+    9: "setembro",
+    10: "outubro",
+    11: "novembro",
+    12: "dezembro",
+}
 
 data = datetime.datetime.now()
-dia = str(data.day) + ' de ' + monthdict[data.month]
-URL = 'https://lolesports.com/schedule?leagues=cblol-brazil,lcs,lec,lck,lpl'
-page = requests.get(URL)
+dia = str(data.day) + " de " + monthdict[data.month]
+URL = "https://lolesports.com/schedule?leagues=cblol-brazil,lcs,lec,lck,lpl"
 
-session = HTMLSession()
-r = session.get(URL)
-
-r.html.render()
-
-r.html.search('E')[0]
-
-soup = BeautifulSoup(page.content, 'lxml')
-#tags = soup.select('div[class="EventMatch"]')
-results = soup.find_all('div', class_="browserupgrade")
-print(results)
-#print(page.content)
+#matches = connect_and_fetch(URL)
+update_csv([])
+#for day in matches[0].next_siblings:
+#    print(day.text)
+#    print()
